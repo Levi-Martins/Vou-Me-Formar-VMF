cba4cbcb9b6abf417ab64a3453473f90422f17c8
Levi Martins <99222268+Levi-Martins@users.noreply.github.com>
2025-12-27 16:16:38 -0300

    [backend] Adjustment to the login functionality in the security filter.

--- src/main/java/com/smd/ufccursos/domain/DTO/request/ElectiveRequirementDTO.java
+++ src/main/java/com/smd/ufccursos/domain/DTO/request/ElectiveRequirementDTO.java
@@ -1,9 +1,11 @@
 package com.smd.ufccursos.domain.DTO.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ElectiveRequirementDTO {
     private Integer semester;
+    @JsonProperty("minEletivasRequired")
     private Integer minRequired;
 }
--- src/main/java/com/smd/ufccursos/infra/security/SecurityFilter.java
+++ src/main/java/com/smd/ufccursos/infra/security/SecurityFilter.java
@@ -6,7 +6,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,32 +18,36 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-  private final TokenService tokenService;
+    private final TokenService tokenService;
 
-  private final PostgresUserRepository userRepository;
+    private final PostgresUserRepository userRepository;
 
-  public SecurityFilter(TokenService tokenService, PostgresUserRepository userRepository) {
-    this.tokenService = tokenService;
-    this.userRepository = userRepository;
-  }
+    public SecurityFilter(TokenService tokenService, PostgresUserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
 
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-      FilterChain filterChain) throws ServletException, IOException {
-      var token = this.recoverToken(request);
-      if (token != null) {
-        var login = tokenService.validateToken(token);
-        UserDetails user = userRepository.findByLogin(login).get();
-        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-      }
-      filterChain.doFilter(request, response);
-  }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+            if (login != null && !login.isEmpty()) {
+                UserDetails user = userRepository.findByLogin(login).orElse(null);
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 
-  private String recoverToken(HttpServletRequest request) {
-    var authHeader = request.getHeader("Authorization");
-    if(authHeader == null) return null;
-    return authHeader.replace("Bearer ", "");
-  }
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
 }
